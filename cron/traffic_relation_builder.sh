20253666e247f576ae7603c682a7076df51dbb03
Alex Waldrop <awaldro1@wm.com>
2018-05-24 08:31:54 -0500

    added cron

---
+++ cron/traffic_relation_builder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Building Traffic Relationships Part 1"
+php ../index.php cron buildTrafficRelation
+echo "Building Traffic Relationships Part 1"
+php ../index.php cron buildTrafficRelationNull
+echo "All Done..."
