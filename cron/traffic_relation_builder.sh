8b3d06fbb17dce9fa9755230e4d9c47f97e2e686
Alex Waldrop <awaldro1@wm.com>
2018-05-24 08:45:02 -0500

    test

--- cron/traffic_relation_builder.sh
+++ cron/traffic_relation_builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Building Traffic Relationships Part 1"
 php ../index.php cron buildTrafficRelation
-echo "Building Traffic Relationships Part 1"
+echo "Building Traffic Relationships Part 2"
 php ../index.php cron buildTrafficRelationNull
 echo "All Done..."
